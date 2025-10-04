bfe866f8da6183a607378c228f6e8366be1ddf06
Mostafiz <mostafiz4372@gmail.com>
2025-10-04 22:58:16 +0600

    created project routes

--- src/modules/project/project.controllers.ts
+++ src/modules/project/project.controllers.ts
@@ -10,11 +10,36 @@ const createProject = async (req:Request, res:Response) => {
 
 // Get All project
 const getAllProject = async (req:Request, res:Response) => {
-    const result = await ProjectServices.createProject();
-    res.status(201).json({status: true, message: "Project Created Successfull"})
+    const result = await ProjectServices.getAllProject();
+    res.status(200).json({status: true, message: "All Project Get Successfull"})
+    
+}
+
+// Get project by id
+const getProjectById = async (req:Request, res:Response) => {
+    const result = await ProjectServices.getProjectById();
+    res.status(200).json({status: true, message: "Single Project Get Successfull"})
+    
+}
+
+// Update project by id
+const UpdateProjectById = async (req:Request, res:Response) => {
+    const result = await ProjectServices.getAllProject();
+    res.status(200).json({status: true, message: "Project Updated Successfull"})
+    
+}
+
+// Delete project By Id
+const DeleteProjectById = async (req:Request, res:Response) => {
+    const result = await ProjectServices.getAllProject();
+    res.status(200).json({status: true, message: "Delete Project Successfull"})
     
 }
 
 export const ProjectControllers = {
-    createProject
+    createProject,
+    getAllProject,
+    getProjectById,
+    UpdateProjectById,
+    DeleteProjectById
 }
--- src/modules/project/project.route.ts
+++ src/modules/project/project.route.ts
@@ -5,5 +5,9 @@ import { ProjectControllers } from "./project.controllers";
 const router = Router();
 
 router.post("/create-project", ProjectControllers.createProject)
+router.get("/", ProjectControllers.getAllProject)
+router.get("/:id", ProjectControllers.getProjectById)
+router.patch("/:id", ProjectControllers.UpdateProjectById)
+router.delete("/:id", ProjectControllers.DeleteProjectById)
 
 export const ProjectRoutes = router;
