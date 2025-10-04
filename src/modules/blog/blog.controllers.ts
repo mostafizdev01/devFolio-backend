3d13edb459f64fdc18e27fe8b5d523f60af424f4
Mostafiz <mostafiz4372@gmail.com>
2025-10-04 14:45:11 +0600

    post blog created successfull

--- src/modules/blog/blog.controllers.ts
+++ src/modules/blog/blog.controllers.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from "express";
 import { BlogServices } from "./blog.services";
 
-
+// create blog
 const CrateBlog = async (req:Request, res:Response)=> {
-    const result = await BlogServices.CrateBlog()
-     return res.status(201).json({
-        status: true,
-        message: "Blog card working.."
-     })
+    const result = await BlogServices.CrateBlog(req.body)
+     return res.status(201).json(result)
     
 }
 
--- src/modules/blog/blog.services.ts
+++ src/modules/blog/blog.services.ts
@@ -1,8 +1,14 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "../../config/db";
 
 
-const CrateBlog = async ()=> {
-    console.log("Blogs is clicked..");
-    
+const CrateBlog = async (payload: Prisma.BlogCreateInput) => {
+    const result = await prisma.blog.create({ data: payload })
+    return {
+        success: true,
+        message: "Blog Created Successfull",
+        payload
+    }
 }
 
 export const BlogServices = {
